0606fd69e2bb474a9c439a6bdbae3d93c316e3a7
nnamdi <ago.nnamdi@gmail.com>
2022-09-19 21:24:23 +0100

    gunicorn setup

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 source django/bin/activate
 
+cd /var/lib/jenkins/workspace/django-cicd
+
+python3 manage.py makemigrations
+python manage.py migrate
+
+echo "Migrate Done"
+
+cd /var/lib/jenkins/workspace/django-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+echo "gunicorn has been started"
+
+sudo systemctl status jenkins
+sudo systemctl restart jenkins
